7c02c341372e02d51ed6eb5ecf5be08dc51873d5
Avkash Mandloi <avkash.mandloi@Yash.com>
2020-05-18 18:19:59 +0530

    fix: Fix errors on command

--- bin/index.js
+++ bin/index.js
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
 const yargs = require('yargs');
-const eslint = require('../src/task/eslint');
-const stylelint = require('../src/task/stylelint');
+const eslint = require('../src/tasks/eslint');
+const stylelint = require('../src/tasks/stylelint');
 
 yargs.scriptName("Run fake API")
   .usage('$0 <cmd> [args]')
   .command(eslint)
-  .command(stylelint)
   .help()
   .argv
