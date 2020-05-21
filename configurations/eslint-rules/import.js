0f0334db694a2c49fd36a02ab0472197453b947e
Avkash Mandloi <avkash.mandloi@Yash.com>
2020-05-21 16:10:36 +0530

    feat: Add more eslint rules

--- configurations/eslint-rules/import.js
+++ configurations/eslint-rules/import.js
@@ -1,7 +1,7 @@
 module.exports = {
     "rules": {
         "import/newline-after-import": ["error", {"count": 1}],
-        "import/no-extraneous-dependencies": ["error", {"devDependencies": ["**/*.test.js", "**/*.spec.js", "**/webpack*.js", "examples/**/*.js"]}],
+        "import/no-extraneous-dependencies": ["error", {"devDependencies": ["test/**/*.js", "**/webpack*.js", "examples/**/*.js"]}],
         "import/order": ["error", {"groups": [["builtin", "external"], ["parent", "sibling", "index"]], "newlines-between": "always"}],
         "import/extensions": ["error", "always", { "js": "never" }],
         "import/named": "error",
